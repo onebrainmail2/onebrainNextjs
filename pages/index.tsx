e181511fa803978aca14b6b12e1703930f8ac348
onebrainmail2 <131781693+onebrainmail2@users.noreply.github.com>
2023-04-25 16:02:36 +0200

    three folders

---
+++ pages/index.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from 'react'
+import HomepageLayout from '@/components/HomepageLayout'
+import BrainAnimationLogo from '@/components/BrainAnimationLogo'
+import CountDownTimer from '@/components/CountDownTimer'
+import PlayerSection from '@/components/PlayerSection'
+import Link from 'next/link'
+const Home: FC = () => {
+  const countdownDate = new Date("May 17, 2023 09:00:00");
+  return (
+    <HomepageLayout headTitle="OneBrain AI">
+      <section>
+        <div className="container">
+          <div className="row-ordered">
+            <div className="col-lg-8 col-sm-12 col-xs-12 brain-text">
+              <h1>Your Company's Genius</h1>
+              <h2>All your<wbr /> knowledge based apps supported by one<wbr /> AI</h2>
+              <p>From all chat history in Slack, to all company content in Google Drive. Connect your company workspace apps and retrieve anything you need straight from your AI Colleague.</p>
+            </div>
+            <BrainAnimationLogo />
+          </div>
+        </div>  
+        <div className="container">
+          <div className="row text-center">
+            <div className="col-lg-12 col-sm-12 col-xs-12">
+              <h2>The AI Knowledge Base Revolution BETA begins in:</h2>
+              {/* Display the countdown timer in an element */}
+              <CountDownTimer countdownDate={countdownDate} />
+            </div>
+          </div>
+        </div>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="d-inline">
+              <Link href="https://tally.so/r/mJqjgd">
+                <span className="join-the-waitlist">Join the waitlist</span>
+              </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br /><br />
+        <br />
+        <br />
+      </section>
+      <PlayerSection title="Instantly receive all answers. All knowledge supported by your AI from chat history." playerId='player1' videoId='DAACfneP7fY'/>
+      <PlayerSection title="Quickly need site analytic stats or comparisons? Just ask." playerId='player2' videoId='UALRgeYdjXg' />
+      <PlayerSection title="Need available times to quickly schedule a meeting? Your company genius knows." playerId='player3' videoId='4N9A2h76YKM' />
+      <PlayerSection title="Request document links and make edit requests easily." playerId='player4' videoId='PinEsrZePvk' />
+      <PlayerSection title="Call info from your sales playbook without needing to memorize it." playerId='player5' videoId='1YTRr2zod1w'/>
+      <PlayerSection title="Need candidate comparisons? Just a question away." playerId='player6' videoId='0Dwocg9HnOw'/>
+    </HomepageLayout>
+  )
+}
+
+export default Home
